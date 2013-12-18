45797ea5da2147d49b56ab884414a70b5cc4d746
Adrian <abcadrian123@gmail.com>
2013-12-18 19:33:22 +0100

    game9

---
+++ db/migrate/20131218170810_create_entities.rb
@@ -0,0 +1,13 @@
+class CreateEntities < ActiveRecord::Migration
+  def change
+    create_table :entities do |t|
+      t.binary :image
+      t.string :latitude
+      t.string :longitude
+      t.string :mime_type
+      t.string :filename
+
+      t.timestamps
+    end
+  end
+end
