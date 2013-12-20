fa029f55034e75d22eaac74a7be08956d7a6a1ef
Adrian <abcadrian123@gmail.com>
2013-12-20 11:01:58 +0100

    music

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,9 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 140 }, :format => {:with => /^https:\/\/soundcloud\.com/m, :multiline => true }
   validates :user_id, presence: true
+  validates_length_of :content, :minimum => 30
 
   # Returns microposts from the users being followed by the given user.
   def self.from_users_followed_by(user)
